82fc0e9fb809bbd2c98debf3c742d29a025614c0
vudinhquang <vudinhquangk53@gmail.com>
2022-07-28 00:26:06 +0900

    Compile

---
+++ 2.typescript/greeter.js
@@ -0,0 +1,5 @@
+function greeter(person) {
+    return "Hello, " + person;
+}
+var user = "Es6";
+document.body.textContent = greeter(user);
---
+++ 2.typescript/greeter.ts
@@ -0,0 +1,7 @@
+function greeter(person: string) {
+    return "Hello, " + person;
+}
+
+let user = "Es6";
+
+document.body.textContent = greeter(user);
