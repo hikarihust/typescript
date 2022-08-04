956d1f7e7b1e68993e506b5d2f715836f669d1c9
vudinhquang <vudinhquangk53@gmail.com>
2022-08-05 00:59:26 +0900

    Module systemjs v0.19.31-v0.19.43

--- 2.typescript/mjs/07_module/Test.js
+++ 2.typescript/mjs/07_module/Test.js
@@ -1,15 +1,16 @@
-/// <reference path="Validation.ts" />
-/// <reference path="LettersOnlyValidator.ts" />
-/// <reference path="ZipCodeValidator.ts" />
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const ZipCodeValidator_1 = require("./ZipCodeValidator");
+const LettersOnlyValidator_1 = require("./LettersOnlyValidator");
 // Some samples to try
 let strings = ["Hello", "98052", "101"];
 // Validators to use
 let validators = {};
-validators["ZIP code"] = new Validation.ZipCodeValidator();
-validators["Letters only"] = new Validation.LettersOnlyValidator();
+validators["ZIP code"] = new ZipCodeValidator_1.ZipCodeValidator();
+validators["Letters only"] = new LettersOnlyValidator_1.LettersOnlyValidator();
 // Show whether each string passed each validator
-for (let s of strings) {
+strings.forEach((s) => {
     for (let name in validators) {
         console.log(`"${s}" - ${validators[name].isAcceptable(s) ? "matches" : "does not match"} ${name}`);
     }
-}
+});
