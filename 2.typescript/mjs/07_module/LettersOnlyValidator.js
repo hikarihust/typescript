7cafa9570c66745979928045640bf4aa93e4d1c5
vudinhquang <vudinhquangk53@gmail.com>
2022-08-05 12:48:15 +0900

    Module without systemjs

--- 2.typescript/mjs/07_module/LettersOnlyValidator.js
+++ 2.typescript/mjs/07_module/LettersOnlyValidator.js
@@ -1,10 +1,6 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.LettersOnlyValidator = void 0;
 const lettersRegexp = /^[A-Za-z]+$/;
-class LettersOnlyValidator {
+export class LettersOnlyValidator {
     isAcceptable(s) {
         return lettersRegexp.test(s);
     }
 }
-exports.LettersOnlyValidator = LettersOnlyValidator;
