02bdaedb0630cb7a3a2a5d190f2a8a36b9c71060
vudinhquang <vudinhquangk53@gmail.com>
2022-08-06 16:01:20 +0900

    JQuery

--- 3.shopping/mjs/shopping.js
+++ 3.shopping/mjs/shopping.js
@@ -1,7 +1,17 @@
 define(["require", "exports", "./product-repository"], function (require, exports, product_repository_1) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
+    var MElement;
+    (function (MElement) {
+        MElement.ELM_LIST_PRODUCT = "#list-product";
+    })(MElement || (MElement = {}));
     let productRepository = new product_repository_1.ProductRepository();
     let products = productRepository.getItems();
-    console.log(productRepository.showItemsInHTML());
+    // Hiển thị danh sách sản phẩm
+    function showListProduct() {
+        $(MElement.ELM_LIST_PRODUCT).html(productRepository.showItemsInHTML());
+    }
+    jQuery(function () {
+        showListProduct();
+    });
 });
--- 3.shopping/ts/shopping.ts
+++ 3.shopping/ts/shopping.ts
@@ -1,7 +1,18 @@
 import { ProductRepository } from './product-repository';
 import { Product } from './product';
 
+namespace MElement {
+	export const ELM_LIST_PRODUCT : string = "#list-product";
+}
+
 let productRepository = new ProductRepository();
 let products : Product[] =  productRepository.getItems();
 
-console.log(productRepository.showItemsInHTML());
+// Hiển thị danh sách sản phẩm
+function showListProduct() : void{
+	$(MElement.ELM_LIST_PRODUCT).html(productRepository.showItemsInHTML());
+}
+
+jQuery(function() { 
+    showListProduct();
+})
